5f456c866bb8f62cde8da7e9cd9fcbbd8de358e9
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-05-27 16:56:09 -0300

    initial

---
+++ android/app/src/main/kotlin/com/example/paper_draw/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.paper_draw
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:paper_draw/src/my_app.dart';
+
+void main() {
+  runApp(
+    const MyApp(),
+  );
+}
